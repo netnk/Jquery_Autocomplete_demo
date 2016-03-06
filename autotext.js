23d66dcf0b8af2ec860da3abd34de9a9efd04714
netnk <netnk0000@gmail.com>
2016-03-06 12:21:52 +0800

    Create autotext.js

---
+++ autotext.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  search_autotext();
+  
+});
+
+
+
+function search_autotext() {
+    $.ajax({ 
+        url: "booklist.ashx",
+        data: { method: "search_autotext" },
+                type: "POST",
+                dataType: "text",
+                success: function (data) { 
+                  var test1 = JSON.parse(data);
+                  $("#search_text").autocomplete({
+                      source: test1
+                     });
+                }
+    });
+}
